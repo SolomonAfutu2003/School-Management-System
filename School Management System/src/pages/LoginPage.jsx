94b2f9ec5ea7fae95be7e99ee674b8dd9c473e85
SolomonTheCoder <solomonafutu1@gmail.com>
2025-05-29 16:24:46 +0100

    first commit

---
+++ School Management System/src/pages/LoginPage.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <section className='h-screen flex bg-white'>
+            {/* Background-image */}
+            <div className='bg-[url("/Background.png")] bg-no-repeat bg-center bg-cover w-[50%]'>
+                dfef
+            </div>
+
+            {/* Login-section */}
+            <div className='w-[50%] flex justify-center items-center'>
+                <div className='flex flex-col items-center gap-5'>
+
+                    {/* Logo */}
+                    <div className='text-btn text-5xl font-header font-bold'>
+                        <h1>LOGO</h1>
+                    </div>
+
+                    {/* header-text */}
+                    <div className='flex flex-col gap-3 text-center font-header'>
+                        <h2 className='font-semibold text-3xl'>Log in to your account</h2>
+                        <p className='font-normal text-base'>Welcome back! Please enter your details.</p>
+                    </div>
+                    <form className='flex flex-col gap-5'>
+
+                        {/* Email-input */}
+                        <div className='flex flex-col'>
+                            <label className='m-0'>Email</label>
+                            <input className="border-2 focus:border-red-500 outline-0 rounded-sm py-2.5 px-3 w-90 border-[#D1D5DB]" type="email" name="" id="" placeholder='Email' />
+                        </div>
+
+                        {/* Password-input */}
+                        <div className='flex flex-col'>
+                            <label>Password</label>
+                            <input className="border-2 focus:border-red-500 outline-0 rounded-sm py-2.5 px-3 w-90 border-[#D1D5DB]" type="password" name="" id="" placeholder='Password' />
+                        </div>
+
+                        <div className='flex justify-end text-btn'>
+                            <span>Forgot Password</span>
+                        </div>
+
+                        <div className='flex justify-center text-white text-base font-semibold'>
+                            <button className='bg-btn py-5 px-4.5 w-90 rounded-sm'>Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default LoginPage
